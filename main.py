30244b631a9c609e79f7780955896018c0bf885d
DevonJohnson10 <825759@mystma.org>
2022-09-21 08:46:19 -0500

    2

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-def greaterThan():
-    if
-    else
-def lessThan():
-    if
-    else
-def equalTo():
-    if
-    else
-def greaterOrEqual():
-    if
-    else
-def lessOrEqual():
-    if
-    else
+def greaterThan(a,b):
+    if a>b:
+        return True
+    else:
+        return False
+def lessThan(c,d):
+    if c<d:
+        return True
+    else:
+        return False
+def equalTo(e,f):
+    if e==f:
+        return True
+    else:
+        return False
+def greaterOrEqual(g,h):
+    if g>=h:
+        return True
+    else:
+        return False
+def lessOrEqual(i,j):
+    if i<=j:
+        return True
+    else:
+        return False
+
+print(greaterThan(3,6))
+print(lessThan(6,2))
+print(equalTo(5,5))
+print(greaterOrEqual(2,6))
+print(lessOrEqual(8,3))
